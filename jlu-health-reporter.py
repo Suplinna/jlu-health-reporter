5d74550518f46749d11d6c02ab8eda2bbb5ba8c8
Suplinna <51396970+Suplinna@users.noreply.github.com>
2021-10-27 01:03:43 +0800

    Update jlu-health-reporter.py

--- jlu-health-reporter.py
+++ jlu-health-reporter.py
@@ -11,7 +11,7 @@ TIMEOUT = 800
 # time between worker thread start
 INTERVAL = 2
 # random delay to lower the load of ehall
-RAND_DELAY = 30*60
+RAND_DELAY = 3*60
 
 def runTask(task):
 	for _ in range(RETRIES):
